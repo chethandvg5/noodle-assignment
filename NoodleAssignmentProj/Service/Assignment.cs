ee16f490d1bb8555db0acb2bed4aae99a6e18e8e
LAPTOP-IVNV7JPF\CHETHAN <chethandvg@gmail.com>
2022-11-18 08:43:29 +0530

    Assignment completed

---
+++ NoodleAssignmentProj/Service/Assignment.cs
@@ -0,0 +1,18 @@
+﻿using NoodleAssignmentProj.Models;
+using NoodleAssignmentProj.Service.IService;
+using System;
+
+namespace NoodleAssignmentProj.Service
+{
+    public class Assignment : IAssignment
+    {
+        public IEnumerable<DemoClass> FetchDemoData(string[] designations) =>
+             Enumerable.Range(1, 5).Select(index => new DemoClass
+            {
+                ID= index,
+                Name="Chethan "+index.ToString(),
+                Description = designations[Random.Shared.Next(designations.Length)]
+             })
+            .ToArray();
+}
+}
